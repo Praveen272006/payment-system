1c91bd7b3705e28a96598b2f4beef0bdcc2a9524
Praveen272006 <praveenkumarv2712006@gmail.com>
2026-04-13 10:28:12 +0530

    Initial Payment CI/CD commit

---
+++ PaymentService.java
@@ -0,0 +1,10 @@
+public class PaymentService {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Payment System Started...");
+
+        while (true) {
+            Thread.sleep(5000);
+            System.out.println("Processing Payments...");
+        }
+    }
+}
